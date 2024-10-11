695afd05ba3ef9e3e8edab87dcf6687da5abea50
Joshua Blais <josh@joshblais.com>
2024-10-11 10:43:11 -0600

    zenmode fix

--- nvim/lua/plugins/zen-mode/init.lua
+++ nvim/lua/plugins/zen-mode/init.lua
@@ -4,10 +4,18 @@ return {
     lazy = false,
     opts = {
       window = {
-        backdrop = 0.95,
-        width = 120,
-        height = 1,
-        options = {},
+        backdrop = 1,  -- Fully opaque
+        width = 100,
+        height = 0.85,
+        options = {
+          signcolumn = "no",
+          number = false,
+          relativenumber = false,
+          cursorline = false,
+          cursorcolumn = false,
+          foldcolumn = "0",
+          list = false,
+        },
       },
       plugins = {
         options = {
@@ -32,8 +40,13 @@ return {
           font = "+4",
         },
       },
-      on_open = function(win) end,
-      on_close = function() end,
+      on_open = function(win)
+        vim.cmd("highlight ZenBg guibg=#2E3440")
+        vim.api.nvim_win_set_option(win.win, "winhighlight", "Normal:ZenBg")
+      end,
+      on_close = function()
+        vim.cmd("highlight clear ZenBg")
+      end,
     }
   }
 }
